1685dbe8d536c48d15e27efaa92b5562ba7d8ba2
André Corrêa <andre-ncs@hotmail.com>
2021-02-25 23:19:17 -0300

    Configurado para requisições http e banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const app = require('express')()
+const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db
+
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
+
+// bcrypt-nodes
+// jwt-simple
+// moment
+// mongoose
+// node-schedule
+// passport
+// passport-jwt
